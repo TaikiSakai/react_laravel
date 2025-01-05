9ce93a7201b899e99db2455a61f873f9ec8441c0
Taiki <s.taiki0301@gmail.com>
2025-01-05 16:26:28 +0900

    update vite settings

--- src/front/vite.config.ts
+++ src/front/vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import route from '@generouted/react-router'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    mhr: {
-        host: 'localhost',
-    }
   }
 })
