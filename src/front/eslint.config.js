8fad3409a790476a03e648ff04441b2b6fecb2cc
Taiki <s.taiki0301@gmail.com>
2025-01-05 22:00:59 +0900

    update eslint settings

--- src/front/eslint.config.js
+++ src/front/eslint.config.js
@@ -1,28 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{ts,jsx,tsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      pluginReactConfig,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react/destructuring-assignment': 'error', // Props などの分割代入を強制
+      'react/function-component-definition': [
+        // コンポーネントの定義方法をアロー関数に統一
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
       ],
+      'react/hook-use-state': 'error', // useState の返り値の命名を [value, setValue] に統一
+      'react/jsx-boolean-value': 'error', // boolean 型の Props の渡し方を統一
+      'react/jsx-fragments': 'error', // React Fragment の書き方を統一
+      'react/jsx-curly-brace-presence': 'error', // Props と children で不要な中括弧を削除
+      'react/jsx-no-useless-fragment': 'error', // 不要な React Fragment を削除
+      'react/jsx-sort-props': 'error', // Props の並び順をアルファベット順に統一
+      'react/self-closing-comp': 'error', // 子要素がない場合は自己終了タグを使う
+      'react/jsx-pascal-case': 'error', // コンポーネント名をパスカルケースに統一
+      'react/no-danger': 'error', // dangerouslySetInnerHTML を許可しない
+      'react/prop-types': 'off', // Props の型チェックは TS で行う & 誤検知があるため無効化
+
+      "max-len": ["error", { code: 80 }], // 行の文字数制限を 80 文字に設定
+      "quotes": ["error", "single", { avoidEscape: true }], // シングルクォーテーションを使用、エスケープを避ける場合は例外
+      "semi": ["error", "always"], // セミコロンを必須にする
     },
   },
-)
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReactConfig,
+  pluginReactJSXRuntime,
+];
