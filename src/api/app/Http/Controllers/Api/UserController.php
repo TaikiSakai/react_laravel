7784ac6865a7302e5b4ead45ff33e1bcd06aa38b
Taiki <s.taiki0301@gmail.com>
2025-01-10 14:58:27 +0900

    add: add create to user_controller

---
+++ src/api/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\BaseController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends BaseController
+{
+    public function index()
+    {
+        $users = User::all();
+        return $this->sendResponse('Successfully fetched', $users);
+    }
+
+    public function create (Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+
+        $data = request()->all();
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', [$validator->errors()]);
+        }        
+
+        $user = User::create($request->all());
+
+        if ($user->save()) {
+            return $this->sendResponse('User Created', $user);
+        }
+    }
+}
