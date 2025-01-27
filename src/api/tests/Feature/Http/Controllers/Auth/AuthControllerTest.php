93b9c9c403cb3b9316140513886bf401a8a70c8a
Taiki <s.taiki0301@gmail.com>
2025-01-27 23:51:50 +0900

    add: add authcontroller test

---
+++ src/api/tests/Feature/Http/Controllers/Auth/AuthControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+
+
+}
